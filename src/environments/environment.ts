b861418033711b243a37a03882f4588b50c2ae06
AndresTS <andres91411@gmail.com>
2025-06-11 15:16:14 -0600

    Update wallet display and environment configurations
    Applied decimal truncation credit and debit columns for consistent formatting in the wallet component. Updated the environment file to use production wallet service URLs and standardized object syntax for clarity and maintainability.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,16 @@ import { FirebaseOptions } from '@firebase/app-types';
 export const environment = {
   production: true,
   apis: {
-   apiUrl: 'https://ecosystemfx.net/',
+    apiUrl: 'https://ecosystemfx.net/',
     accountService: 'https://account.recycoin.net/api/v1',
     accountServiceSignalR: 'https://account.recycoin.net/ticketHub',
     systemConfigurationService: 'https://configuration.recycoin.net/api/v1',
     inventoryService: 'https://inventory.recycoin.net/api/v1',
-    walletService: 'http://localhost:5235/api/v1',
-    coinPayment: 'https://www.coinpayments.net/index.php'
+    walletService: 'https://wallet.recycoin.net/api/v1',
+    coinPayment: 'https://www.coinpayments.net/index.php',
   },
   openAI: {
-    apiKey: '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'
+    apiKey: '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',
   },
   tokens: {
     coinPayment: 'bfd40db8f711397a6c5b7653175afc38',
@@ -20,7 +20,7 @@ export const environment = {
     systemConfigurationService: 'eco-key8ZgMhRytu-Jrv1FU1rZSw2jM-FaBP!ou!sJNBITT3tA63GBrrQiVe3zvS',
     inventoryService: 'eco-keyLd5DU5faBWLfLrE1ATUK0c1qpvSci1x5TvFkDVw3FEM7JO30Jm!zXyB4w',
     walletService: 'eco-keypFvQnUOko=r4/G!chia5Fe2-6OU?2YNYqAPWlaiN!uYrZIdwoUNv9P4d7',
-    clientID: 'eco-keyhFvQoUOk=r6/F!chia2Fe1-8OU?4YNWqAVWlaiN!tYrWIdvoUMv8Q6d6'
+    clientID: 'eco-keyhFvQoUOk=r6/F!chia2Fe1-8OU?4YNWqAVWlaiN!tYrWIdvoUMv8Q6d6',
   },
   coinPaymentConfiguration: {
     publicApiKey: '2a4ae9a2a58b59f4cf3cecf76e89f04155ccdcca4dc0c76b8665cf852cc127c2',
@@ -29,16 +29,16 @@ export const environment = {
     reset: '1',
     cmd: '_pay_simple',
     success_url: 'https://ecosystemfx.net/#/conpayment-confirmation',
-    format: 'json'
-  }
+    format: 'json',
+  },
 };
 
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyBVM9OkEJa_rdAID5ydC8gGKjNaU6fFzQI",
-  authDomain: "ecosystem-6b056.firebaseapp.com",
-  projectId: "ecosystem-6b056",
-  storageBucket: "ecosystem-6b056.appspot.com",
-  messagingSenderId: "1077107109427",
-  appId: "1:1077107109427:web:bbe6268a7b4f1831717d46",
-  measurementId: "G-64EF1WMHB8"
+  apiKey: 'AIzaSyBVM9OkEJa_rdAID5ydC8gGKjNaU6fFzQI',
+  authDomain: 'ecosystem-6b056.firebaseapp.com',
+  projectId: 'ecosystem-6b056',
+  storageBucket: 'ecosystem-6b056.appspot.com',
+  messagingSenderId: '1077107109427',
+  appId: '1:1077107109427:web:bbe6268a7b4f1831717d46',
+  measurementId: 'G-64EF1WMHB8',
 };
