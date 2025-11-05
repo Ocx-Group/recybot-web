97ad47c31977c1090790b1bc1e956bae1b8f1098
AndresTS <andres91411@gmail.com>
2025-11-05 11:59:47 -0600

    feat(billing-purchase): integrate ProductsComponent and RouterLink
    Added ProductsComponent and RouterLink to the BillingPurchaseComponent imports for enhanced functionality and navigation. This change allows for better integration of product-related features within the billing purchase flow.

--- src/app/client/billing-purchase/billing-purchase.component.ts
+++ src/app/client/billing-purchase/billing-purchase.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import {ProductsComponent} from "@app/client/products/products.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
     selector: 'app-billing-purchase',
     templateUrl: './billing-purchase.component.html',
     styleUrls: ['./billing-purchase.component.scss'],
     standalone: true,
-    imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, ProductsComponent, RouterLink],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class BillingPurchaseComponent implements OnInit {
